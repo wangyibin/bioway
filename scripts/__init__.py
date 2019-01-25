d990ae8d81dd2d8db2e1f611364e5fedcd2379e2
YibinWang <738339463@qq.com>
2019-01-25 13:58:15 +0800

    create script directory

---
+++ scripts/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# @Time: 2019/1/25 13:56
---
+++ scripts/stat_sge_info.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# @Time: 2019/1/25 13:57
+
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import os
+import sys
+
+
+def stat_sge_used(mode='r',user="all"):
+    info = os.popen("qstat -u '*'")
+    info_dict = {}
+    head = info.readline().strip().split()
+
+    for line in info:
+        data = line.strip().split()
+
+        if len(data) >= 8 and data[4] == mode:
+            if data[3] not in info_dict:
+                info_dict[data[3]] = 0
+            info_dict[data[3]] += int(data[8])
+
+    info_dict = sorted(info_dict.items(),
+                key=lambda x:x[1],reverse=True)
+
+    total = 0
+    for item in info_dict:
+        print("{0}\t{1}".format(item[0],item[1]))
+        total += item[1]
+
+    print("Total:\t{}".format(total))
+
+
+
+if __name__ == "__main__":
+    stat_sge_used()
